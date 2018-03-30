44ffe45d8612c59f85e757987f9873ca69895de5
vardhan <vardhan@vardhan-Lenovo-G500>
2018-03-30 10:30:53 +0530

    integrating with mysql

--- spring-cloud-task/src/main/java/com/example/demo/SpringCloudTaskApplication.java
+++ spring-cloud-task/src/main/java/com/example/demo/SpringCloudTaskApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.cloud.task.listener.annotation.AfterTask;
+import org.springframework.cloud.task.listener.annotation.BeforeTask;
+import org.springframework.cloud.task.listener.annotation.FailedTask;
+import org.springframework.cloud.task.repository.TaskExecution;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableTask
@@ -12,13 +17,33 @@ public class SpringCloudTaskApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudTaskApplication.class, args);
 	}
-	
-	public static class GreetingTask implements CommandLineRunner{
+
+	@Bean
+	public GreetingTask greetingTask() {
+		return new GreetingTask();
+	}
+
+	public static class GreetingTask implements CommandLineRunner {
 
 		@Override
-		public void run(String... args) throws Exception {
-			System.out.println("Message: " + "Hello!");
+		public void run(String... arg0) throws Exception {
+			System.out.println("Message : " + arg0[1]);
 		}
-		
+
+		@AfterTask
+		public void onTaskEnd(TaskExecution arg0) {
+			System.out.println("Task Completed Sucessfully");
+		}
+
+		@FailedTask
+		public void onTaskFailed(TaskExecution arg0, Throwable arg1) {
+			System.out.println("Task Failed");
+		}
+
+		@BeforeTask
+		public void onTaskStartup(TaskExecution arg0) {
+			System.out.println("Task started");
+		}
+
 	}
 }
